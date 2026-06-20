fb6ff8aa47bba499512baec14c3721e70f0401b9
Mahmoud Diab <47752204+mahmouddiab01@users.noreply.github.com>
2026-06-20 20:06:53 +0300

    Add documentation to TestClass for Selenium WebDriver setup

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -9,5 +9,5 @@ public class TestClass {
     public void test(){
         WebDriver driver = new ChromeDriver();
     }
-    // This is a test class for Selenium WebDriver using TestNG framework. It initializes a ChromeDriver instance to perform web automation tasks. The test method can be expanded to include specific test cases and assertions as needed.
+    //test
 }
